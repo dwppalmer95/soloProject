64833b142f18f15d91ee7ebf000a90d10d447e64
david palmer <dwppalmer95@gmail.com>
2022-03-23 20:30:36 -0500

    progress setting up webpack. leftoff: setup production environment

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js?x/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
